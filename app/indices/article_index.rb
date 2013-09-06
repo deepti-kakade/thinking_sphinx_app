8a57473c32ffb0dcdbbb712c4a6ba5c32ae6e7cc
deepti-webonise <deepti@weboniselab.com>
2013-09-06 17:25:15 +0530

    searching works properly

--- app/indices/article_index.rb
+++ app/indices/article_index.rb
@@ -3,6 +3,5 @@ ThinkingSphinx::Index.define :article, :with => :active_record do
   indexes :name, :sortable => true
   indexes comments.content, :as => :comment_content
   indexes author.first_name, :as => :author_name
-
   has author_id, created_at
 end
