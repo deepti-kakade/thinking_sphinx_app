60a4110de6ce0b1dee4bc708a1e3ff3b111dfa07
deepti-webonise <deepti@weboniselab.com>
2013-09-06 14:15:57 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThinkingSphinxApp::Application.config.secret_key_base = 'bdacbdf79e1cd4a30179cddcf2ea719a201903cfe4799f4d04ea989d6583321bdd40a6a5a122e2ffdd7d55a626df8b9df1d970d2fe7f1a10f586f09ae159e271'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThinkingSphinxApp::Application.config.session_store :cookie_store, key: '_thinking_sphinx_app_session'
